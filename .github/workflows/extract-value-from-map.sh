28246e7c0d0fb90debb0cd8828fd39984c5618f3
Doug Linning <62891957+dlinning-jockey@users.noreply.github.com>
2022-07-18 14:43:27 -0500

    Added deploy to azure workflow (#1)
    * changed deploy_env -> default
    
    * Removed unnecessary word
    
    * Added deploy to azure workflow

--- .github/workflows/extract-value-from-map.sh
+++ .github/workflows/extract-value-from-map.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: ./[script].sh -d? "DEPLOY_ENV" -k "KEY" -m "BRANCH_MAP_JSON"
+# Usage: ./[script].sh -d? "DEFAULT_VALUE" -k "KEY" -m "BRANCH_MAP_JSON"
 # -d is optional
 # -b and -k are required if -d not provided
 
@@ -11,14 +11,14 @@ while getopts k:m:d:v flag; do
     case "${flag}" in
     k) key=${OPTARG} ;;
     m) map=${OPTARG} ;;
-    d) deploy_env=${OPTARG} ;;
+    d) default=${OPTARG} ;;
     v) verbose="true" ;;
     esac
 done
 
 # Check if we have a provided deployenv
-if [ ! -z "$deploy_env" ]; then
-    echo "$deploy_env"
+if [ ! -z "$default" ]; then
+    echo "$default"
     exit 0 # Exit early, we got our value
 fi
 
