9d66e95968b1aa18f986e40ee1b1de9a939143c2
Doug Linning <doug.linning@jockey.com>
2022-07-18 13:50:39 -0500

    Initial commit

---
+++ .github/workflows/extract-value-from-map.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Usage: ./[script].sh -d? "DEPLOY_ENV" -k "KEY" -m "BRANCH_MAP_JSON"
+# -d is optional
+# -b and -k are required if -d not provided
+
+verbose=""
+
+# Extract Args to nicer variables
+while getopts k:m:d:v flag; do
+    case "${flag}" in
+    k) key=${OPTARG} ;;
+    m) map=${OPTARG} ;;
+    d) deploy_env=${OPTARG} ;;
+    v) verbose="true" ;;
+    esac
+done
+
+# Check if we have a provided deployenv
+if [ ! -z "$deploy_env" ]; then
+    echo "$deploy_env"
+    exit 0 # Exit early, we got our value
+fi
+
+# Validate flags
+if [ -z "$key" ] || [ -z "$map" ]; then
+    echo "Missing -k[ey] or -m[ap] flag. Both are required."
+    exit 1
+fi
+
+if [ "$verbose" != "" ]; then
+    echo "Provided Map:"
+    echo "$map" | jq -r "."
+fi
+
+echo "$map" | jq -r ".$key"
